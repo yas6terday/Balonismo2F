c51530f7e6d1367cb2a13fbea75eb0d14cf73de5
Yasmim <126568485+yas6terday@users.noreply.github.com>
2023-08-13 10:28:42 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,50 @@
-import Classes.Balao;
 import java.util.Scanner;
+
+import Classes.Piloto;
+import Classes.Balao;
+import Classes.Pessoa;
+
 public class Main {
     public static void main(String[] args) {
+ Scanner tcd = new Scanner(System.in);
+ int x =0;
+        while  (x == 0){
 
-        Balao b1 = new Balao();
-        String resultado = b1.voar();
-        System.out.println(resultado);
+            System.out.println("Bem vindo a agencia de balão, digite 1 se quiser adicionar um balão, digite 2 se quiser ver o balão já registrado, digite 3 se quiser registrar um piloto, digite 4 se quiser ver o piloto já regisrado, digite 5 se quiser registrar um passageiro, digite 6 se quiser ver o passsageiro já registrado");
 
-        b1.setId();
-        b1.setRegistro();
-        b1.setCor();
-        b1.setTipo();
-        b1.setCapacidade();
-        b1.setAltura();
-        b1.setPorte();
-        b1.setPeso();
+             x = tcd.nextInt();
 
-        Scanner tcd = new Scanner(System.in);
+            if (x == 1){
+                 Balao b1 = new Balao();
+                 b1.registroBalao();
+            }
+            else if(x == 2){
+                Balao b2 = new Balao();
+                b2.registradoBalao();
+            }
+            else if (x == 3){
+                Piloto p1 = new Piloto();
+                p1.registroPiloto();
+            }
+            else if (x == 4){
+                Piloto p2 = new Piloto();
+                p2.registradoPiloto();
+            }
+            else if (x == 5){
+                Pessoa pe1 = new Pessoa();
+                pe1.registroPessoa();
+            }
+            else if (x == 6){
+                Pessoa pe2 = new Pessoa();
+                pe2.registradoPessoa();
+            }
 
-        Balao b2 = new Balao();
-        System.out.println("Digite o ID, o Registro, a cor, o tipo, a Capacidade, a Altura máxima, o Porte e o Peso máximo permito do balão");
-        b2.setId();
-        b2.setRegistro();
-        b2.setCor();
-        b2.setTipo();
-        b2.setCapacidade();
-        b2.setAltura();
-        b2.setPorte();
-        b2.setPeso();
-    }
+            System.out.println("Deseja continuar no Sistema? " + "0 para sim / 1 para não ");
+             x = tcd.nextInt();
+        }
+        
+
+   
+}
+
 }
