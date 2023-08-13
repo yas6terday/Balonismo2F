4b383dd6dcac42bca12e91deab4ca59dc673ddfa
Yasmim <126568485+yas6terday@users.noreply.github.com>
2023-08-13 10:30:11 -0300

    Update Balao.java

--- src/Classes/Balao.java
+++ src/Classes/Balao.java
@@ -1,11 +1,10 @@
 package Classes;
 
+import java.util.Scanner;
 public class Balao {
 
     private int id;
-
     private String registro;
-
     private String cor;
     private String tipo;
     private int capacidade;
@@ -13,63 +12,100 @@ public class Balao {
     private String porte;
     private double peso;
 
-    public void setId(){
+    public void setId(int id) {
         this.id = id;
     }
-    public int getId(){
+
+    public int getId() {
         return id;
     }
 
-    public void setRegistro(){
+    public void setRegistro(String registro) {
         this.registro = registro;
     }
-    public String getRegistro(){
+
+    public String getRegistro() {
         return registro;
     }
 
-    public void setCor(){
+    public void setCor(String cor) {
         this.cor = cor;
     }
-    public String getCor(){
+
+    public String getCor() {
         return cor;
     }
 
-    public void setTipo(){
+    public void setTipo(String tipo) {
         this.tipo = tipo;
     }
-    public String getTipo(){
+
+    public String getTipo() {
         return tipo;
     }
 
-    public void setCapacidade(){
+    public void setCapacidade(int capacidade) {
         this.capacidade = capacidade;
     }
-    public int getCapacidade(){
+
+    public int getCapacidade() {
         return capacidade;
     }
 
-    public void setAltura(){
+    public void setAltura(double altura) {
         this.altura = altura;
     }
-    public double getAltura(){
+
+    public double getAltura() {
         return altura;
     }
 
-    public void setPorte(){
+    public void setPorte(String porte) {
         this.porte = porte;
     }
-    public String getPorte(){
+
+    public String getPorte() {
         return porte;
     }
 
-    public void setPeso(){
+    public void setPeso(double peso) {
         this.peso = peso;
     }
-    public double getPeso(){
+
+    public double getPeso() {
         return peso;
     }
 
     public String voar(){
         return "voando";
     }
-}
+
+    public void registroBalao(){
+        System.out.println("Digite o id, o registro, a cor, o tipo, a capacidade, a altura, o porte e o peso suportado pelo balão:");
+        Scanner tcd = new Scanner(System.in);
+        this.id = tcd.nextInt();
+        this.registro = tcd.next();
+        this.cor= tcd.next();
+        this.tipo = tcd.next();
+        this.capacidade = tcd.nextInt();
+        this.altura = tcd.nextDouble();
+        this.porte = tcd.next();
+        this.peso = tcd.nextDouble();
+        System.out.println("id: " + this.id + " Registro: " + this.registro + " Cor: " + this.cor + " Tipo: " + this.tipo + " Capacidade Máxima: " + this.capacidade
+         + " Altura Máxima: " + this.altura + " Porte: " + this.porte + " Peso Máximo: " + this.peso );
+    }
+
+        public void registradoBalao(){
+        System.out.println("Balão já registrado");
+        this.id = 254554;
+        this.registro = "sfsdf";
+        this.cor = "azul";
+        this.tipo = "comum";
+        this.capacidade = 5;
+        this.altura = 100.00;
+        this.porte = "grande";
+        this.peso = 110.00;
+         System.out.println("id: " + this.id + " Registro: " + this.registro + " Cor: " + this.cor + " Tipo: " + this.tipo + " Capacidade Máxima: " + this.capacidade
+         + " Altura Máxima: " + this.altura + " Porte: " + this.porte + " Peso Máximo: " + this.peso );
+    }
+    }
