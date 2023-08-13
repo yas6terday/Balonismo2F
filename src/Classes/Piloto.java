252a1b12d46b4fcfd2dc3ef332d5dade50de767c
Yasmim <126568485+yas6terday@users.noreply.github.com>
2023-08-13 10:30:38 -0300

    Update Piloto.java

--- src/Classes/Piloto.java
+++ src/Classes/Piloto.java
@@ -1,32 +1,51 @@
 package Classes;
 
+import java.util.Scanner;
+
 public class Piloto {
-    private String nome;
-    private int CPF;
-    private int registroAnac;
+     private String nome;
+     private int CPF;
+     private int registroAnac;
 
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-    public String getNome(){
-        return nome;
+     public void setNome(String nome){
+          this.nome = nome;
+     }
+     public String getNome(){
+          return nome;
+     }
+
+     public void setCPF(int CPF){
+          this.CPF = CPF;
+     }
+     public int getCPF(){
+          return CPF;
+     }
+
+     public void setRegistroAnac(int registroAnac){
+          this.registroAnac = registroAnac;
+     }
+     public int getRegistroAnac(){
+          return registroAnac;
+     }
+
+     public String pilotando(){
+          return "Pilotando";
+     }
+
+     public void registroPiloto(){
+        System.out.println("Digite o nome, o cpf e o registro da anac do piloto");
+        Scanner tcd = new Scanner(System.in);
+        this.nome = tcd.next();
+        this.CPF = tcd.nextInt();
+        this.registroAnac = tcd.nextInt();
+        System.out.println("Nome do piloto: " + this.nome + " CPF do piloto: " + this.CPF + " Registro ANAC do piloto: " + this.registroAnac);
     }
 
-    public void setCPF(int CPF){
-        this.CPF = CPF;
-    }
-    public int getCPF(){
-        return CPF;
-    }
-
-    public void setRegistroAnac(String RG){
-        this.registroAnac = registroAnac;
-    }
-    public int getRegistroAnac(){
-        return registroAnac;
-    }
-
-    public String pilotando(){
-        return "Pilotando";
+     public void registradoPiloto(){
+        System.out.println("Piloto registrado");
+        this.nome = "Barão Vermelho";
+        this.CPF = 54654654;
+        this.registroAnac = 798546;
+        System.out.println("Nome do piloto: " + this.nome + " CPF do piloto: " + this.CPF + " Registro ANAC do piloto: " + this.registroAnac);
     }
 }
